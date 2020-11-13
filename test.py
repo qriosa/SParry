caf98d5d3f6bc1a3298c700de9cab37b823d3b6b
“chegnxiae <chegnxiang@163.com>
2020-11-13 11:13:45 +0800

    添加了部分全源测试数据

--- test.py
+++ test.py
@@ -69,9 +69,9 @@ def test(filename = None, n = 1000, m = 5000, l = 1, r = 20):
 	useCUDAFalse = 'noUseCUDA'
 	ans = []
 	# srclist = [i for i in range(n)]
-	# srclist = None
+	srclist = None
 	# srclist = 232
-	srclist = 2
+	# srclist = 2
 	# srclist = [1,2,3,12,13,17]
 	# srclist = [20,123,1114,5098,6111,9914,23,345,123,345,435,67,234,124,456,768,34,234,456,78,234,56,678,89,123,456,678,423,576,8964,6489,1999,2437,1031,5436,6522,1456,2345]
 	# srclist = [2, 5, 12, 45, 45, 23, 87, 145, 567, 368, 325, 463, 168, 1276, 2416, 1567, 23, 4567, 2352, 3456, 2878, 2978, 1983]
@@ -82,12 +82,13 @@ def test(filename = None, n = 1000, m = 5000, l = 1, r = 20):
 	for i in range(len(method)):
 		logger.info(f'begin to calc. method = {method[i]}, useCUDA = {useCUDA[i]}, pathRecordBool = False, directed = {directed}, srclist = {np.array(srclist)}')
 		
-		ans.append(calc(graph = filename, graphType = 'edgeSet', method = method[i], useCUDA = useCUDA[i], directed = directed, pathRecordBool = True, srclist = srclist, block=(512, 2, 1), grid = (1, 1)))
+		ans.append(calc(graph = filename, graphType = 'CSR', method = method[i], useCUDA = useCUDA[i], srclist = srclist))
 		
 		logger.info(f'begin to check')
 		
-		PRINT(check(ans[0].dist, ans[i].dist, method[0], method[i])) # 检测两个答案是否相等
+		PRINT(check(ans[0].dist, ans[i].dist, method[0], method[i] + (useCUDATrue if useCUDA[i] else useCUDAFalse))) # 检测两个答案是否相等
 		PRINT(f'{method[i]}_{useCUDATrue if useCUDA[i] else useCUDAFalse} time cost = ' + str(ans[i].timeCost)) # 计算用时
+		print(ans[i].display())
 		
 		# print(ans[i].dist.reshape(len(srclist), n))
 		# print(ans[i].dist)
@@ -104,4 +105,4 @@ if __name__ == '__main__':
 	else:
 		filename = None
 
-	test(filename = filename, n = 1000, m = 2000, l = 3, r = 30)
+	test(filename = filename, n = 100, m = 1000, l = 1, r = 100)
